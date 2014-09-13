49aafb3a9a6090a8f13e9227a6e9fab3ea032ba0
David Ratajczak <david@mockra.com>
2014-09-13 09:21:10 -0700

    adds js for nav styling

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -11,4 +11,18 @@ $(function() {
     $('div[data-tab-target="' + $(this).attr('href') + '"]').show();
     e.preventDefault();
   });
+
+  var path = window.location.pathname;
+  if (path == '/index.html' || path == '/' || path == '/index') {
+  } else if (path == '/contact.html' || path == '/contact') {
+    $('ul#nav a#contact').addClass('active');
+  } else if (path == '/about.html' || path == '/about') {
+    $('ul#nav a#about').addClass('active');
+  } else if (path == '/services.html' || path == '/services') {
+    $('ul#nav a#services').addClass('active');
+  } else if (path == '/projects.html' || path == '/projects') {
+    $('ul#nav a#projects').addClass('active');
+  } else {
+    $('ul#nav a#blog').addClass('active');
+  }
 });
